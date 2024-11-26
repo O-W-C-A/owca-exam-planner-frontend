132e19145ad8adedf96aaee69dc45f2e96533619
George Luta <george.luta@webitfactory.io>
2024-11-26 11:31:09 +0200

    CORS request without CORS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
-import Calendar from './components/Calendar';
+'use client';
+import { useEffect } from 'react';
+import useDataStore from './state/courses-state';
 
 export default function Home() {
-  return <Calendar />;
+  const { data, isLoading, error, fetchData } = useDataStore();
+
+  useEffect(() => {
+    fetchData(); // Obținem datele când componenta se montează
+  }, [fetchData]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h1>Posts:</h1>
+      <ul>
+        {data.map((post) => (
+          <li key={post.numeProfesor}>
+            {post.title}
+            <p>{post.status}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  // return <Calendar />;
 }
