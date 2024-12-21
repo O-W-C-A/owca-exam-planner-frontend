b9116505ba47402fcd0d7eb5ac20d2c67981c601
Ruebled <dan.canevschii@gmail.com>
2024-12-21 18:05:55 +0200

    [FEATURE] UI Testing in Cypress

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,45 @@
+describe('Testare Formular de Login', () => {
+    beforeEach(() => {
+      cy.visit('/login'); 
+    });
+  
+    it('Autentificare pentru Student - Students1@university.edu', () => {
+      cy.get('input[name="email"]').type('Students1@university.edu');
+      cy.get('input[name="password"]').type('hashedpassword1');
+      cy.get('button').contains('Submit').click();
+      
+      cy.url().should('include', '/dashboard');
+      cy.get('a').contains('Logout').click();
+      cy.get('button').contains('Yes').click();
+      
+    //   cy.contains('Welcome, John Doe').should('be.visible');
+    });
+  
+    // it('Autentificare reușită pentru Profesor - Professors1@university.edu', () => {
+    //   cy.get('input[name="email"]').type('Professors1@university.edu');
+    //   cy.get('input[name="password"]').type('hashedpassword3');
+    //   cy.get('button').contains('Submit').click();
+  
+    //   cy.url().should('include', '/dashboard');
+    // //   cy.contains('Welcome, Alice Johnson').should('be.visible');
+    // });
+  
+    // it('Autentificare reușită pentru Admin - admin@university.edu', () => {
+    //   cy.get('input[name="email"]').type('admin@university.edu');
+    //   cy.get('input[name="password"]').type('hashedpassword5');
+    //   cy.get('button').contains('Submit').click();
+  
+    //   cy.url().should('include', '/admin-dashboard');
+    // //   cy.contains('Welcome, Admin Adminson').should('be.visible');
+    // });
+  
+    // // Test negativ - credențiale greșite
+    // it('Autentificare eșuată cu credențiale greșite', () => {
+    //   cy.get('input[name="email"]').type('Students1@university.edu');
+    //   cy.get('input[name="password"]').type('wrongpassword');
+    //   cy.get('button').contains('Submit').click();
+  
+    //   cy.contains('Invalid email or password').should('be.visible');
+    // });
+  });
+  
