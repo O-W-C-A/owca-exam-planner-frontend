15c90e1f84b69e1a4d54945a97df657bc4d5c321
Ruebled <dan.canevschii@gmail.com>
2024-12-23 04:38:45 +0200

    Add expired token check

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { clearAuthData } from './tokenUtils';
 
 // Use the URL passed via the environment variable
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -10,10 +11,23 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = Cookies.get('authToken');
+  
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+
+// Add response interceptor to handle token expiration
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      clearAuthData();
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
