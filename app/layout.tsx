821649b3fb0264cdf210d62eaaea7a37da94b349
Ruebled <dan.canevschii@gmail.com>
2025-01-18 01:08:28 +0200

    [LINT] Fix font import lint issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from '@/contexts/UserContext';
 
@@ -7,6 +7,12 @@ const inter = Inter({
   display: 'block',
   preload: true,
   weight: ['400', '500', '600', '700'],
+});
+
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
 });
 
 const globalMetadata = {
@@ -18,13 +24,9 @@ export const metadata = globalMetadata;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={`${inter.className} ${notoSans.className}`}>
       <head>
         <link rel="icon" href="/usv_logo.ico" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
         <title>{globalMetadata.title}</title>
       </head>
       <body className="antialiased bg-blue-50">
